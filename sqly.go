f168165d4310c7571e23e7b880219395100e788b
Martin Bruse <zondolfin@gmail.com>
2025-01-01 15:08:53 +0100

    ..

---
+++ sqly.go
@@ -0,0 +1,234 @@
+package sqly
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+type DB struct {
+	sqlx.DB
+}
+
+func (db *DB) Upsert(ctx context.Context, structPointer any, overwrite bool) error {
+	return Upsert(ctx, db, structPointer, overwrite)
+}
+
+func (db *DB) CreateTableIfNotExists(ctx context.Context, prototype any) error {
+	return CreateTableIfNotExists(ctx, db, prototype)
+}
+
+func (db *DB) Beginy() (*Tx, error) {
+	tx, err := db.Beginx()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &Tx{*tx}, nil
+}
+
+type Tx struct {
+	sqlx.Tx
+}
+
+func (tx *Tx) Upsert(ctx context.Context, structPointer any, overwrite bool) error {
+	return Upsert(ctx, tx, structPointer, overwrite)
+}
+
+func (tx *Tx) CreateTableIfNotExists(ctx context.Context, prototype any) error {
+	return CreateTableIfNotExists(ctx, tx, prototype)
+}
+
+type StackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+func Open(driverName string, dataSourceName string) (*DB, error) {
+	db, err := sqlx.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	db.MapperFunc(func(s string) string { return s })
+	return &DB{*db}, nil
+}
+
+func Upsert(ctx context.Context, execer sqlx.ExecerContext, structPointer any, overwrite bool) error {
+	val := reflect.ValueOf(structPointer)
+	if val.Kind() != reflect.Ptr {
+		return errors.Errorf("%v is not a reflect.Ptr", structPointer)
+	}
+	val = val.Elem()
+	if val.Kind() != reflect.Struct {
+		return errors.Errorf("%v is not a pointer to a reflect.Struct", structPointer)
+	}
+	typ := val.Type()
+	cols := []string{}
+	qmarks := []string{}
+	params := []any{}
+	var primaryKeyFieldToSet *reflect.Value
+	for fieldIndex := 0; fieldIndex < typ.NumField(); fieldIndex++ {
+		field := typ.Field(fieldIndex)
+		skip := false
+		if field.IsExported() {
+			for _, tag := range strings.Split(field.Tag.Get("sqly"), ",") {
+				fieldVal := val.Field(fieldIndex)
+				if tag == "pkey" && fieldVal.CanInt() && fieldVal.Int() == 0 {
+					primaryKeyFieldToSet = &fieldVal
+					skip = true
+				}
+			}
+			if !skip {
+				cols = append(cols, fmt.Sprintf("`%s`", field.Name))
+				qmarks = append(qmarks, "?")
+				params = append(params, val.Field(fieldIndex).Interface())
+			}
+		}
+	}
+	replace := ""
+	if overwrite {
+		replace = "OR REPLACE "
+	}
+	res, err := execer.ExecContext(ctx, fmt.Sprintf("INSERT %sINTO `%s` (%s) VALUES (%s)", replace, typ.Name(), strings.Join(cols, ","), strings.Join(qmarks, ",")), params...)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if primaryKeyFieldToSet != nil {
+		lastID, err := res.LastInsertId()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		log.Printf("setting pkey to %v", lastID)
+		primaryKeyFieldToSet.SetInt(lastID)
+	}
+	return nil
+}
+
+type index struct {
+	cols   []string
+	unique bool
+}
+
+var (
+	uniqueWithRegexp = regexp.MustCompile(`uniqueWith\\((.*)\\)`)
+	indexWithRegexp  = regexp.MustCompile(`indexWith\\((.*)\\)`)
+)
+
+func CreateTableIfNotExists(ctx context.Context, execer sqlx.ExecerContext, prototype any) error {
+	val := reflect.ValueOf(prototype)
+	if val.Kind() != reflect.Struct {
+		return errors.Errorf("%v is not a reflect.Struct", prototype)
+	}
+	typ := val.Type()
+	primaryKeyCol := ""
+	primaryKeySQLType := ""
+	cols := []string{}
+	sqlTypes := []string{}
+	indices := []index{}
+	for fieldIndex := 0; fieldIndex < typ.NumField(); fieldIndex++ {
+		field := typ.Field(fieldIndex)
+		if field.IsExported() {
+			sqlType := ""
+			switch field.Type.Kind() {
+			case reflect.String:
+				sqlType = "TEXT"
+			case reflect.Uint:
+				fallthrough
+			case reflect.Uint8:
+				fallthrough
+			case reflect.Uint16:
+				fallthrough
+			case reflect.Uint32:
+				fallthrough
+			case reflect.Uint64:
+				fallthrough
+			case reflect.Int:
+				fallthrough
+			case reflect.Int8:
+				fallthrough
+			case reflect.Int16:
+				fallthrough
+			case reflect.Int32:
+				fallthrough
+			case reflect.Int64:
+				sqlType = "INTEGER"
+			case reflect.Float32:
+				sqlType = "REAL"
+			case reflect.Float64:
+				sqlType = "REAL"
+			case reflect.Bool:
+				sqlType = "INTEGER"
+			default:
+				return errors.Errorf("%v isn't of a supported type", field)
+			}
+			isPkey := false
+			for _, tag := range strings.Split(field.Tag.Get("sqly"), ",") {
+				switch tag {
+				case "unique":
+					index := index{
+						cols:   []string{field.Name},
+						unique: true,
+					}
+					index.cols = append(index.cols, field.Name)
+					indices = append(indices, index)
+				case "index":
+					index := index{
+						cols:   []string{field.Name},
+						unique: false,
+					}
+					index.cols = append(index.cols, field.Name)
+					indices = append(indices, index)
+				case "pkey":
+					isPkey = true
+					primaryKeyCol = field.Name
+					primaryKeySQLType = sqlType
+				default:
+					if match := uniqueWithRegexp.FindStringSubmatch(tag); match != nil {
+						indices = append(indices, index{
+							cols:   strings.Split(match[1], ";"),
+							unique: true,
+						})
+					} else if match = indexWithRegexp.FindStringSubmatch(tag); match != nil {
+						indices = append(indices, index{
+							cols:   strings.Split(match[1], ";"),
+							unique: false,
+						})
+					}
+				}
+				if !isPkey {
+					cols = append(cols, field.Name)
+					sqlTypes = append(sqlTypes, sqlType)
+				}
+			}
+		}
+	}
+	if primaryKeyCol == "" {
+		return errors.Errorf("%v doesn't have a PRIMARY KEY (field tagged `sqly:\"pkey\"`)", prototype)
+	}
+	if _, err := execer.ExecContext(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%s` (`%s` %s PRIMARY KEY)", typ.Name(), primaryKeyCol, primaryKeySQLType)); err != nil {
+		return errors.WithStack(err)
+	}
+	for colIndex, col := range cols {
+		if _, err := execer.ExecContext(ctx, fmt.Sprintf("ALTER TABLE `%s` ADD COLUMN `%s` %s", typ.Name(), col, sqlTypes[colIndex])); err != nil && !strings.Contains(err.Error(), "duplicate column name") {
+			return errors.WithStack(err)
+		}
+	}
+	for _, index := range indices {
+		unique := ""
+		if index.unique {
+			unique = "UNIQUE "
+		}
+		escapedCols := make([]string, len(index.cols))
+		for colIndex, col := range index.cols {
+			escapedCols[colIndex] = fmt.Sprintf("`%s`", col)
+		}
+		if _, err := execer.ExecContext(ctx, fmt.Sprintf("CREATE %sINDEX IF NOT EXISTS `%s.%s` ON `%s` (%s)", unique, typ.Name(), strings.Join(index.cols, ","), typ.Name(), strings.Join(escapedCols, ","))); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
---
+++ sqly_test.go
@@ -0,0 +1,124 @@
+package sqly
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	_ "modernc.org/sqlite"
+)
+
+var (
+	ctx = context.Background()
+)
+
+func withDB(t *testing.T, f func(db *DB)) {
+	t.Helper()
+	db, err := Open("sqlite", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f(db)
+
+}
+
+type testStruct struct {
+	unexported int
+	Uint       uint
+	Uint8      uint8
+	Uint16     uint16
+	Uint32     uint32
+	Uint64     uint64
+	Int        int `sqly:"pkey"`
+	Int8       int8
+	Int16      int16
+	Int32      int32
+	Int64      int64
+	String     string
+	Bool       bool
+	Float32    float32
+	Float64    float64
+}
+
+type indexedTestStruct struct {
+	Id            int `sqly:"pkey"`
+	Indexed       int `sqly:"index"`
+	Unique        int `sqly:"unique"`
+	ThreeIndexed1 int
+	ThreeIndexed2 int
+	ThreeIndexed3 int `sqly:"indexWith(ThreeIndexed1;ThreeIndexed2)"`
+	ThreeUnique1  int
+	ThreeUnique2  int
+	ThreeUnique3  int `sqly:"uniqueWith(ThreeUnique1;ThreeUnique2)"`
+}
+
+func yeserr(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Fatalf("got nil, wanted some error")
+	}
+}
+
+func noerr(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		if err, ok := err.(StackTracer); ok {
+			t.Logf("%s", err.StackTrace())
+		}
+		t.Fatal(err)
+	}
+}
+
+func TestCRU(t *testing.T) {
+	withDB(t, func(db *DB) {
+		noerr(t, db.CreateTableIfNotExists(ctx, testStruct{}))
+		wantStr := &testStruct{
+			unexported: 1,
+			Uint:       2,
+			Uint8:      3,
+			Uint16:     4,
+			Uint32:     5,
+			Uint64:     6,
+			Int8:       8,
+			Int16:      9,
+			Int32:      10,
+			Int64:      11,
+			String:     "12",
+			Bool:       true,
+			Float32:    13.0,
+			Float64:    14.0,
+		}
+		noerr(t, db.Upsert(ctx, wantStr, false))
+		if wantStr.Int == 0 {
+			t.Fatal("wanted a new primary key, got 0")
+		}
+		gotStr := &testStruct{}
+		noerr(t, db.Get(gotStr, "SELECT * FROM testStruct WHERE Int = ?", wantStr.Int))
+		if gotStr.unexported != 0 {
+			t.Errorf("got unexported %v, wanted 0", gotStr.unexported)
+		}
+		gotStr.unexported = wantStr.unexported
+		if !reflect.DeepEqual(gotStr, wantStr) {
+			t.Errorf("got %+v, wanted %+v", gotStr, wantStr)
+		}
+	})
+}
+
+func TextIndex(t *testing.T) {
+	withDB(t, func(db *DB) {
+		noerr(t, db.CreateTableIfNotExists(ctx, testStruct{}))
+		wantStr := &indexedTestStruct{
+			Indexed:       1,
+			Unique:        2,
+			ThreeIndexed1: 1,
+			ThreeIndexed2: 2,
+			ThreeIndexed3: 3,
+			ThreeUnique1:  1,
+			ThreeUnique2:  2,
+			ThreeUnique3:  3,
+		}
+		noerr(t, db.Upsert(ctx, wantStr, false))
+		wantStr.Id = 0
+		noerr(t, db.Upsert(ctx, wantStr, true))
+	})
+}
